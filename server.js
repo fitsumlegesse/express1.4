ab4df7962bd67dc87133b51ce9dbe6903fe219d7
Fitsum Legesse <55632280+fitsumlegesse@users.noreply.github.com>
2020-06-21 15:54:48 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+var express  = require('express');
+var data = require('./data/test.json');
+
+var app = express();
+const PORT = 3000;
+
+app.set('view engine','ejs');//sent views to EJS
+
+//this is how a route is built in Express
+app.get('/', (req,res)=>{
+  var title = "Our Home Page";
+  res.render('pages/index',{title:title});
+});
+
+app.get('/users', (req,res)=>{
+  var title = "Our Home Page";
+  res.render('users/index',{
+    title:title,
+    users:data
+  });
+});
+
+app.listen(PORT,()=>{
+  console.log(`App is running on port ${PORT}`);
+  //console.log(data);
+});
